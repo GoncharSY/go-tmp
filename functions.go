7b7194d8c57c93ab115770eecefdf74366e8c444
gonchar.sy <gonchar.sy.dev@gmail.com>
2022-08-30 21:53:16 +0300

    Добавил функцию для вывода версии модуля

--- functions.go
+++ functions.go
@@ -2,6 +2,11 @@ package gotmp
 
 import "fmt"
 
+// SayVersion prints version of module.
+func SayVersion() {
+	fmt.Println("v1.1.0")
+}
+
 // SayHello prints message 'Hello!'.
 func SayHello() {
 	fmt.Println("Hello!")
